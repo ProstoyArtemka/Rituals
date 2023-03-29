9ef070fa147dfa0950afc39f35053dbc554e5fa4
ProstoyArtemka <ProstoyArtemka@yandex.ru>
2023-03-29 22:45:08 +0700

    Init

---
+++ src/main/java/prostoyartemka/ru/rituals/RItuals.java
@@ -0,0 +1,16 @@
+package prostoyartemka.ru.rituals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RItuals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
